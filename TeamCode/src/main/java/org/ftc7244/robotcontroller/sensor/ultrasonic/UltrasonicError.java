31584903000a5fe745aeac2e0f5fec6fa1ac1e81
Eeshwar Krishnan <32040858+Eeshwar-Krishnan@users.noreply.github.com>
2018-10-06 09:45:12 -0400

    Finished the Ultrasonic Error class to return error from the 4 ultrasonic sensors on the sides of the robot

--- TeamCode/src/main/java/org/ftc7244/robotcontroller/sensor/ultrasonic/UltrasonicError.java
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/sensor/ultrasonic/UltrasonicError.java
@@ -3,18 +3,33 @@ package org.ftc7244.robotcontroller.sensor.ultrasonic;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class UltrasonicError {
+    SickUltrasonic leftFront, leftBack, rightFront, rightBack;
     UltrasonicSide side;
-    SickUltrasonic ultrasonicFront;
-    SickUltrasonic ultrasonicBack;
-    public UltrasonicError(UltrasonicSide side){
-        this.side = side;
-    }
-    public void start(HardwareMap map){
-        if(side == UltrasonicSide.LEFT){
+    public UltrasonicError(SickUltrasonic frontLeftSensor, SickUltrasonic backLeftSensor, SickUltrasonic frontRightSensor, SickUltrasonic backRightSensor){
+        this.side  = side;
+        leftFront = frontLeftSensor;
+        leftBack = backLeftSensor;
+        rightFront = frontRightSensor;
+        rightBack = backRightSensor;
 
+    }
+    public void start(SickUltrasonic.Mode mode){
+        leftFront.setMode(mode);
+        leftBack.setMode(mode);
+        rightBack.setMode(mode);
+        rightFront.setMode(mode);
+    }
+    public double getError(UltrasonicSide side){
+        if(side == UltrasonicSide.LEFT){
+            return leftFront.getUltrasonicLevel() - leftBack.getUltrasonicLevel();
+        }else if(side == UltrasonicSide.RIGHT){
+            return rightFront.getUltrasonicLevel() - rightBack.getUltrasonicLevel();
+        }else{
+            return -1;
         }
     }
-    public double getError(){
-        return 0;
+    public enum UltrasonicSide{
+        LEFT,
+        RIGHT
     }
 }
--- TeamCode/src/main/java/org/ftc7244/robotcontroller/sensor/ultrasonic/UltrasonicSide.java
+++
@@ -1,6 +0,0 @@
-package org.ftc7244.robotcontroller.sensor.ultrasonic;
-
-public enum UltrasonicSide {
-    LEFT,
-    RIGHT
-}
