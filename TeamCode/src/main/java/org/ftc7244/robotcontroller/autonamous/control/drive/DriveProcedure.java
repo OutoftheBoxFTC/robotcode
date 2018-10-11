dc1c2f999109dd9cd36c276503e7b4e5bf416cad
chiraagChakravarthy <beaverduck99@gmail.com>
2018-10-10 21:08:39 -0400

    Began control system skeleton Universal orientation True position vs compounded

---
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/autonamous/control/drive/DriveProcedure.java
@@ -0,0 +1,20 @@
+package org.ftc7244.robotcontroller.autonamous.control.drive;
+
+public class DriveProcedure {
+    private double d;
+    private double r;
+    private Orientation orientation;
+
+    public DriveProcedure(double x, double y, Orientation orientation){
+        this.orientation = orientation;
+
+    }
+
+    public double getRotationTarget(){
+
+    }
+
+    public double getDistanceTarget(){
+
+    }
+}
