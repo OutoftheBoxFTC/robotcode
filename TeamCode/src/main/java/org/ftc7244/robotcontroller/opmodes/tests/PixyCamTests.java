38f5f8d5202752cc3b6522f8d90a93cfad645364
Eeshwar Krishnan <32040858+Eeshwar-Krishnan@users.noreply.github.com>
2018-11-24 10:38:07 -0500

    Started Test for Pix2

--- TeamCode/src/main/java/org/ftc7244/robotcontroller/opmodes/tests/PixyCamTests.java
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/opmodes/tests/PixyCamTests.java
@@ -2,26 +2,56 @@ package org.ftc7244.robotcontroller.opmodes.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 
 import org.ftc7244.robotcontroller.sensor.pixycam.PixycamProvider;
 import org.ftc7244.robotcontroller.sensor.pixycam.SampleProvider;
 @TeleOp
+/*
+Bytes    16-bit word    Description
+        ----------------------------------------------------------------
+        0, 1     y              sync: 0xaa55=normal object, 0xaa56=color code object
+        2, 3     y              checksum (sum of all 16-bit words 2-6, that is, bytes 4-13)
+        4, 5     y              signature number
+        6, 7     y              x center of object
+        8, 9     y              y center of object
+        10, 11   y              width of object
+        12, 13   y              height of object
+        */
 public class PixyCamTests extends OpMode {
     I2cDeviceSynch pixy;
-    PixycamProvider pixyProvider;
-    SampleProvider sampleProvider;
     @Override
     public void init() {
         pixy = hardwareMap.i2cDeviceSynch.get("pixy");
-        pixyProvider = new PixycamProvider(PixycamProvider.Mineral.GOLD, pixy);
-        sampleProvider = new SampleProvider(pixyProvider);
-        while(!sampleProvider.start()){}
+        pixy.setI2cAddress(I2cAddr.create7bit(0x54));
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow (1, 26, I2cDeviceSynch.ReadMode.REPEAT);
+        pixy.setReadWindow(readWindow);
+        pixy.engage();
     }
 
     @Override
     public void loop() {
-        telemetry.addData("Location", sampleProvider.run());
+/*        telemetry.addData("Byte 0", pixy.read8(0));
+        telemetry.addData("Byte 1", pixy.read8(1));
+        telemetry.addData("Byte 2", pixy.read8(2));
+        telemetry.addData("Byte 3", pixy.read8(3));
+        telemetry.addData("Byte 4", pixy.read8(4));
+        telemetry.addData("Byte 5", pixy.read8(5));
+        telemetry.addData("Byte 6", pixy.read8(6));
+        telemetry.addData("Byte 7", pixy.read8(7));
+        telemetry.addData("Byte 8", pixy.read8(8));
+        telemetry.addData("Byte 9", pixy.read8(9));
+        telemetry.addData("Byte 10", pixy.read8(10));
+        telemetry.addData("Byte 11", pixy.read8(11));
+        telemetry.addData("Byte 12", pixy.read8(12));
+        telemetry.addData("Byte 13", pixy.read8(13));
+*/      telemetry.addData("X", twoBytesToInt(pixy.read8(6), pixy.read8(7)));
+        telemetry.addData("Y", twoBytesToInt(pixy.read8(8), pixy.read8(9)));
         telemetry.update();
+    }
+
+    private static int twoBytesToInt(byte b1, byte b2) {
+        return ((b2 << 8) | (b1 & 0xFF));
     }
 }
