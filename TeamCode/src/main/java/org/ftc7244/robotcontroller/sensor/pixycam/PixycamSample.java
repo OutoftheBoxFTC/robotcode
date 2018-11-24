e15996a4d520376f3acadff9da59513f81d869ba
Eeshwar Krishnan <32040858+Eeshwar-Krishnan@users.noreply.github.com>
2018-11-24 15:10:04 -0500

    Added PixycamSample

---
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/sensor/pixycam/PixycamSample.java
@@ -0,0 +1,31 @@
+package org.ftc7244.robotcontroller.sensor.pixycam;
+
+public class PixycamSample {
+
+    PixycamProvider pixy;
+    public PixycamSample(PixycamProvider pixy){
+        this.pixy = pixy;
+    }
+
+    public boolean start(){
+        pixy.start();
+        return pixy.isEngaged();
+    }
+
+    public SampleTransform run(){
+        pixy.update();
+        if(pixy.getX() < 62){
+            return SampleTransform.RIGHT;
+        }else if(pixy.getX() > 189){
+            return SampleTransform.LEFT;
+        }else{
+            return SampleTransform.CENTER;
+        }
+    }
+
+    public enum SampleTransform{
+        LEFT,
+        RIGHT,
+        CENTER
+    }
+}
